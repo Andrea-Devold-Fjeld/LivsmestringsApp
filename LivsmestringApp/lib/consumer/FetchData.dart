f8feb3366f2383c15003c81fef4af4ee0b9cd843
Andrea-Devold-Fjeld <andrea.devold.fjeld@elbits.no>
2025-05-01 15:24:55 +0200

    Remocved category

--- LivsmestringApp/lib/consumer/FetchData.dart
+++ LivsmestringApp/lib/consumer/FetchData.dart
@@ -18,12 +18,12 @@ Future<Datamodel> fetchData(String category) async {
     //final response = await dio.get('http://localhost:8080/$category');
     if(response.statusCode == 200) {
       log("After response: $response ");
-      return Datamodel.fromJson(response.data, category);
+      return Datamodel.fromJson(response.data);
     }
     if (response.statusCode != 200){
       log(response.statusCode as String);
       log(response.headers as String);
-      return Datamodel.fromJson(response.data, category);
+      return Datamodel.fromJson(response.data);
     }
   }catch (e){
     throw Exception('Failed to load data from $category: $e');
